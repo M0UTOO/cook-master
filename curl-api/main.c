d66a50aeb681e49c3e5ca341d135b25b7d77970b
Mil0w0 <lhilderal@myges.fr>
2023-03-20 22:42:23 +0100

    basic C program using curl, wip

--- curl-api/main.c
+++ curl-api/main.c
@@ -11,12 +11,22 @@ TO COMPILE : gcc main.c -o main.exe -lcurl
 int main(int argc, char ** argv){
 
     CURL *curl;
+    CURLcode res;
+ 
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "https://google.com");
 
-    printf("OK");
-
-    curl = curl_easy_init();
-
+    /* Perform the request, res will get the return code */
+    res = curl_easy_perform(curl);
+    /* Check for errors */
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+ 
+    /* always cleanup */
     curl_easy_cleanup(curl);
+  }
     return 0;
 
 }
