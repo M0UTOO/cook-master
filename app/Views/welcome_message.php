3faf036d48ded58045284e960d5acb4e60b3827c
Mil0w0 <lhilderal@myges.fr>
2023-05-06 14:44:50 +0200

    new stuff - grommet icons and can use assets from public folder

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Welcome to CodeIgniter 4!</title>
-    <meta name="description" content="The small framework with powerful features">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/png" href="/favicon.ico">
-
-</head>
+<?= $this->include('layouts/head') ?>
+<?= $this->include('layouts/header') ?>
 <body>
     <?php
 
@@ -20,4 +12,6 @@ use function PHPUnit\Framework\isEmpty;
         }
     ?>
 </body>
+<?= $this->include('layouts/footer') ?>
+
 </html>
