d585ce51d7c9ebd2ff97937838fde8e891d7d36b
Mil0w0 <lhilderal@myges.fr>
2023-05-06 16:56:11 +0200

    mobile header and footer created

--- app/Views/layouts/head.php
+++ app/Views/layouts/head.php
@@ -3,16 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title><?= $title ?></title>
-    <meta name="description" content="The small framework with powerful features">
+    <meta name="description" content="Cookmaster website">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;700&display=swap" rel="stylesheet">
-<!--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
-    <link rel="stylesheet" type="text/css" href=<?php base_url("assets/css/colors.css")?>>
-    <link rel="stylesheet" type="text/css" href=<?php base_url("assets/css/common.css")?>>
+    <link rel="stylesheet" type="text/css" href=<?php echo base_url("assets/css/common.css")?>>
 
 </head>
