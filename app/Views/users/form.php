94105841773c05720de9e5c546bb82d4000271e2
Mil0w0 <lhilderal@myges.fr>
2023-05-06 19:37:32 +0200

    wip signIn forms

--- app/Views/users/form.php
+++ app/Views/users/form.php
@@ -6,8 +6,8 @@ $email = "E-mail";
 
 helper('form');
 
-echo form_open("users/create", 'id="user-form"');
-//echo '<form action='. base_url('/users/create') .' method="post" accept-charset="utf-8">';
+echo form_open("users/create", 'id="user-form" class="light-form"');
+    echo "<h2>".$signIn."</h2>";
     echo form_password("password","", 'placeholder=" ' . $password . '"');
     echo form_label("Votre mot de passe", "password");
     echo form_input("email", "", 'placeholder="'. $email . '"');
@@ -15,3 +15,4 @@ echo form_open("users/create", 'id="user-form"');
 
     echo form_submit('',$signIn);
 echo form_close();
+
