d2f03f138c54006bb630481e1a20fbd385338d23
Mil0w0 <lhilderal@myges.fr>
2023-05-28 18:24:33 +0200

    can manage subscriptions

--- app/Controllers/Subscription.php
+++ app/Controllers/Subscription.php
@@ -14,32 +14,68 @@ class Subscription extends BaseController
         return view('subscription/index', $data);
     }
 
-    //Manager and Super admin can
+    //Manager can
     public function create()
     {
+        //todo: pull api and check This WITH DELETE() TOO
+        helper('filesystem');
+
         $data['title'] = "Create a new subscription";
 
         if (!$this->request->is('post')) {
-            return view('subscriptions/create', $data);
+            return view('subscription/form', $data);
         } else {
             $values = $this->request->getPost();
-            $data['message'] = callAPI('/user/', 'post', $values);
-            return redirect()->to('/dashboard/subscriptionManagement')->with('message', $data['message']['message']);
+
+            $values['price'] = (float)$values['price'];
+            $values['maxlessonaccess'] = (int)$values['maxlessonaccess'];
+
+            $picture = $this->request->getFile('picture');
+
+            $data['message'] = callAPI('/subscription/', 'post', $values);
+            $subscriptionID = $data['message']['id'];
+
+            $picture = "img-subscription-".$subscriptionID.".". $picture->getExtension(); //check extension
+
+            $data['state'] = callAPI('/subscription/'.$subscriptionID, 'patch', ['picture' => $picture]);
+
+            if (!$data['state']['error']){
+                $picture->move('./assets/images/subscriptions/', 'img-subscription-'.$subscriptionID.'.'.$picture->getExtension());
+            }
+
+            return redirect()->to('/subscriptions')->with('message', $data['message']['message']);
         }
     }
 
     public function edit($id){
 
-        $data['subscription'] = callAPI('/subscription/'.$id, 'get');
-        //TODO: SHOW SIGNUP FORM WITH USER DATA PRE-FILLED
-        return view('subscription/index', $data);
+        $data['title'] = "Edit the subscription";
+        $data['subscription'] = callAPI('/subscription/'.$id, 'get'); //TO DISPLAY THE CURRENT VALUES IN THE FORM
+
+        if (!$this->request->is('post')) {
+            return view('subscription/form', $data);
+        } else {
+            $values = $this->request->getPost();
+            $data['message'] = callAPI('/subscription/'.$id, 'patch', $values);
+            return redirect()->to('/subscription/'.$id)->with('message', $data['message']['message']);
+        }
     }
     public function delete($id){
-        //delete subscriptions from DB:
-        echo $id;
+        helper('filesystem');
+
         $data['message'] = callAPI('/subscription/'.$id, 'delete');
-        return redirect()->to('/dashboard/subscriptionManagement')->with('message', $data['message']['message']);
+
+        if (!$data['message']['error']){
+            delete_files('./assets/images/subscriptions/img-subscription-'.$id, true);
+        }
+
+        return redirect()->to('/subscriptions')->with('message', $data['message']['message']);
     }
 
+    public function show($id){
+        $data['title'] = "Subscription";
+        $data['subscription'] = callAPI('/subscription/'.$id, 'get');
+        return view('subscription/show', $data);
+    }
 
 }
---
+++ app/Views/subscription/edit.php
@@ -0,0 +1,3 @@
+<?php
+echo "<h2>" . $title . "<img alt='logo' class='' src=" . base_url("assets/images/svg/moon-icon.svg") . " /></h2>";
+echo $this->include('subscription/form') ;
