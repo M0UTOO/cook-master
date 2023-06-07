ee72b912dba9543f77823fe213584f29f98ab1c3
Mil0w0 <lhilderal@myges.fr>
2023-06-07 23:21:47 +0200

    can create event but need to save pic somewhere

--- app/Controllers/Event.php
+++ app/Controllers/Event.php
@@ -11,4 +11,51 @@ class Event extends BaseController
         return view('event/index', $data);
     }
 
+
+    public function create()
+    {
+        helper('filesystem');
+
+        $data['title'] = "Create a new event";
+
+        if (!$this->request->is('post')) {
+            return view('event/create', $data);
+        }
+        else
+        {
+            $values = $this->request->getPost();
+            if (isset($values['isinternal'])) {
+                $values['isinternal'] = boolval($values['isinternal']);
+            }
+            if (isset($values['isprivate'])) {
+                $values['isprivate'] = boolval($values['isprivate']);
+            }
+            if (isset($values['defaultpicture'])) {
+                $values['defaultpicture'] = $values['defaultpicture'] = $this->request->getFile('defaultpicture');
+            }
+            if (isset($values['endtime'])) {
+                $values['endtime'] = date("Y-m-d H:i:s", strtotime($values['endtime']));
+            }
+            if (isset($values['starttime'])) {
+                $values['starttime'] = date("Y-m-d H:i:s", strtotime($values['starttime']));
+            }
+
+            $userId = $values['user_id'];
+            unset($values['user_id']);
+
+            $data['message'] = callAPI('/event/'.$userId, 'post', $values);
+
+            /*$lessonID = $data['message']['id'];
+
+            $picture = "img-lesson-".$lessonID.".". $picture->getExtension(); //check extension
+
+            $data['state'] = callAPI('/lesson/'.$lessonID, 'patch', ['picture' => $picture]);
+
+            if (!$data['state']['error']){
+                $picture->move('./assets/images/lessons/', 'img-lesson-'.$lessonID.'.'.$picture->getExtension());
+            }*/
+
+            return redirect()->to('/events')->with('message', $data['message']['message']);
+        }
+    }
 }
--- app/Views/event/form.php
+++ app/Views/event/form.php
@@ -2,46 +2,67 @@
 
 use App\Controllers\LessonGroup;
 
-
     $hidden_input = ['user_id' => session()->get('id')];
     $url = uri_string();
     $url = preg_replace('/[0-9]+/', '', $url);
-    if ($url == "lesson/edit/") {
-        $action = "lesson/edit/".$lesson['idlesson'];
+    if ($url == "event/edit/") {
+        $action = "event/edit/".$event['idevent'];
     } else {
-        $action = "lesson/create";
+        $action = "event/create";
     }
-    echo form_open_multipart($action, 'id="lesson-create-form" class=""', $hidden_input);
+    echo form_open_multipart($action, 'id="event-create-form" class=""', $hidden_input);
 
                 echo '<div class="form-group mb-3">';
-                            echo form_label('Lesson name' , "label-lesson-name");
-                            $value = (isset($lesson) ? $lesson['name'] :'');
-                            echo form_input(['type'  => 'text', 'name'  => 'name', 'class' => 'form-control', 'value' => $value, 'placeholder' => "Name of lesson", 'required' => 'required']);
+                            echo form_label('Event name' , "label-event-name");
+                            $value = (isset($event) ? $event['name'] :'');
+                            echo form_input(['type'  => 'text', 'name'  => 'name', 'class' => 'form-control', 'value' => $value, 'placeholder' => "Name of event", 'required' => 'required']);
                 echo '</div>';
 
                 echo '<div class="form-group mb-3">';
-                echo form_label('Lesson description' , "label-lesson-description");
-                  $value = (isset($lesson) ? $lesson['description'] :'');
-                echo form_input(['type'  => 'textarea', 'name'  => 'description', 'class' => 'form-control', 'value' => $value, 'placeholder' => "Description of lesson", 'required' => 'required']);
+                echo form_label('Event type' , "label-event-type");
+                $value = (isset($event) ? $event['type'] :'');
+                echo form_input(['type'  => 'text', 'name'  => 'type', 'class' => 'form-control', 'value' => $value, 'placeholder' => "Type of event (exemple: Demonstration,Conference,Degustation...)", 'required' => 'required']);
                 echo '</div>';
 
-
-                echo '<div class="form-group mb-3">'; //DIFFICULTY
-                    echo form_label('Lesson difficulty (1-5)' , "label-lesson-difficulty");
-                    $value = (isset($lesson) ? $lesson['difficulty'] :'');
-                    echo form_input(['type'  => 'range', 'min' => 1, 'max' => 5, 'step' => 1, 'name'  => 'difficulty', 'class' => 'form-control', 'value' => $value, 'required' => 'required']);
+                echo '<div class="form-group">';
+                echo form_label('Event start time' , "label-start-time");
+                echo form_input(['type'  => 'datetime-local', 'name'  => 'starttime', 'class' => 'form-control', 'placeholder' => "Contract start date", 'required' => 'required']);
                 echo '</div>';
 
-                echo '<div class="form-group mb-3">'; //CONTENT FILE OR LIN TO YTB VIDEO.
-                    echo form_label('Lesson content' , "label-lesson-content");
-                    $value = (isset($lesson) ? $lesson['content'] :'');
-                    echo form_input(['type'  => 'text', 'name'  => 'content','value' => $value, 'class' => 'form-control']);
+                echo '<div class="form-group">';
+                echo form_label('Event end time' , "label-end-time");
+                echo form_input(['type'  => 'datetime-local', 'name'  => 'endtime', 'class' => 'form-control', 'placeholder' => "Contract end date", 'required' => 'required']);
+                echo '</div>';
+
+                $booleans = [
+                    'isprivate'  => 'Is a private event (customers house)',
+                    'isinternal'    => 'Is an internal event (cooking space)',
+                    'isclosed' => 'Is finished'
+                ];
+                $isChecked = true;
+
+
+                echo "<div class='form-group'>";
+                echo "<label>This event...?</label>";
+                foreach ($booleans as $key => $value){
+                    echo '<div class="form-check">';
+                    echo '<input class="form-check-input" type="checkbox" id="check-'.$key.'" name='. $key .' value="'. $isChecked.'" >';
+                    echo '<label class="form-check-label">'. $value .'</label>';
+                    echo "</div>";
+                }
+                echo "</div>";
+
+
+                echo '<div class="form-group mb-3">';
+                    echo form_label('Event main picture' , "label-event-picture");
+                    $value = (isset($event) ? $event['defaultpicture'] :'');
+                    echo form_input(['type'  => 'file', 'name'  => 'defaultpicture','value' => $value, 'class' => 'form-control']);
                 echo '</div>';
 
                 //APPEAR BUT CAN'T BE MODIFIED BUT BY MANAGER.
                 echo '<div class="form-group mb-3">'; //AUTHOR
-                    echo form_label('Lesson author' , "label-lesson-content");
-                    $value = (isset($author) ? $lesson['author'] :'');
+                    echo form_label('Event creator' , "label-event-creator");
+                    $value = (isset($author) ? $event['author'] :'');
                     echo form_input(['type'  => 'text', 'name'  => 'content','value' => $value, 'class' => 'form-control', 'disabled' =>'disabled'], );
                 echo '</div>';
 
