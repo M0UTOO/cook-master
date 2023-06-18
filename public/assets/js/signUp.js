e0a3d2b7def7865b9744e4f42f62e2714c5dc3a3
Mil0w0 <lhilderal@myges.fr>
2023-06-18 18:13:46 +0200

    creating user with free account by default

--- public/assets/js/signUp.js
+++ public/assets/js/signUp.js
@@ -1,13 +1,39 @@
-function selectSubscription(id, price){
+let submitButton = document.getElementById('submit_btn');
+let form = document.getElementById('signUp-form');
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    console.log('submit button clicked');
+    checkInputs()
+    setCookies()
+    form.submit()
 
-    if (price === 0) {
-        //SUBSCRIPTION IS FREE : NO NEED TO CHECKOUT
-        document.getElementById('sign-up-subscription-input').value = id;
-        console.log("free");
-    } else {
-        if (id > 0) {
-            window.location.href = '/checkout?subscription=' + id;
-       }
+});
+
+
+function setCookies() {
+    let expirationDate = new Date();
+    expirationDate.setTime(expirationDate.getTime() + (15 * 60 * 1000)); // Set expiration to 15 minutes in milliseconds
+
+    let email = document.getElementById('sign-up-email-input').value;
+    document.cookie = "email=" + encodeURIComponent(email) + "; expires=" + expirationDate.toUTCString();
+
+    let firstName = document.getElementById('sign-up-firstname').value;
+    document.cookie = "firstName=" + encodeURIComponent(firstName) + "; expires=" + expirationDate.toUTCString();
+
+    let lastName = document.getElementById('sign-up-lastname').value;
+    document.cookie = "lastName=" + encodeURIComponent(lastName) + "; expires=" + expirationDate.toUTCString();
+
+    let country = document.getElementById('sign-up-country').value;
+    document.cookie = "country=" + encodeURIComponent(country) + "; expires=" + expirationDate.toUTCString();
+
+}
+function checkInputs() {
+    let password = document.getElementById('sign-up-password-input').value;
+
+    if (password.length < 8) {
+        //TODO: MAKE INPUT BORDER RED
+        document.getElementById('sign-up-password-input').style.borderColor = "red";
+        console.log('password too short');
     }
 
 }
