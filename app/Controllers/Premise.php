eee6035bceab29e95c4990d65e4cdca6e56cb341
Mil0w0 <lhilderal@myges.fr>
2023-06-24 19:03:06 +0200

    can view premises in manager dashboard

--- app/Controllers/Premise.php
+++ app/Controllers/Premise.php
@@ -4,77 +4,66 @@ namespace App\Controllers;
 
 class Premise extends BaseController
 {
-    //Everyone can
     public function index()
     {
-        //SHOW ALL INFO ABOUT ALL SUBSCRIPTIONS
-        $data['title'] = "Cookmaster - Subscription";
-        $data['subscriptions'] = callAPI('/subscription/all', 'get');
-        return view('subscription/index', $data);
+        //SHOW ALL INFO ABOUT ALL PREMISE
+        $data['title'] = "Cookmaster - Premises";
+        $data['premises'] = callAPI('/premise/all', 'get');
+       // var_dump($data['premises']);
+        return view('premises/index', $data);
     }
 
     //Manager can
     public function create()
     {
-        //todo: pull api and check This WITH DELETE() TOO
-        helper('filesystem');
 
-        $data['title'] = "Create a new subscription";
+        $data['title'] = "Create a new premise";
 
         if (!$this->request->is('post')) {
-            return view('subscription/form', $data);
+            return view('premise/create', $data);
         } else {
             $values = $this->request->getPost();
 
-            $values['price'] = (float)$values['price'];
-            $values['maxlessonaccess'] = (int)$values['maxlessonaccess'];
+            $values['streetnumber'] = (int)$values['streetnumber'];
 
-            $picture = $this->request->getFile('picture');
+            $data['message'] = callAPI('/premise/', 'post', $values);
 
-            $data['message'] = callAPI('/subscription/', 'post', $values);
-            $subscriptionID = $data['message']['id'];
-
-            $picture = "img-subscription-".$subscriptionID.".". $picture->getExtension(); //check extension
-
-            $data['state'] = callAPI('/subscription/'.$subscriptionID, 'patch', ['picture' => $picture]);
-
-            if (!$data['state']['error']){
-                $picture->move('./assets/images/subscriptions/', 'img-subscription-'.$subscriptionID.'.'.$picture->getExtension());
-            }
-
-            return redirect()->to('/subscriptions')->with('message', $data['message']['message']);
+            return redirect()->to('/premises')->with('message', $data['message']['message']);
         }
     }
 
-    public function edit($id){
+    public function edit($id)
+    {
 
-        $data['title'] = "Edit the subscription";
-        $data['subscription'] = callAPI('/subscription/'.$id, 'get'); //TO DISPLAY THE CURRENT VALUES IN THE FORM
+        $data['title'] = "Edit the premise";
+        $data['premise'] = callAPI('/premise/' . $id, 'get'); //TO DISPLAY THE CURRENT VALUES IN THE FORM
 
         if (!$this->request->is('post')) {
-            return view('subscription/form', $data);
+            return view('premise/edit', $data);
         } else {
             $values = $this->request->getPost();
-            $data['message'] = callAPI('/subscription/'.$id, 'patch', $values);
-            return redirect()->to('/subscription/'.$id)->with('message', $data['message']['message']);
+            $values['streetnumber'] = (int)$values['streetnumber'];
+
+            $data['message'] = callAPI('/premise/' . $id, 'patch', $values);
+
+            return redirect()->to('/premise/' . $id)->with('message', $data['message']['message']);
         }
     }
-    public function delete($id){
-        helper('filesystem');
 
-        $data['message'] = callAPI('/subscription/'.$id, 'delete');
+    public function delete($id)
+    {
 
-        if (!$data['message']['error']){
-            delete_files('./assets/images/subscriptions/img-subscription-'.$id, true);
-        }
+        $data['message'] = callAPI('/premise/' . $id, 'delete');
 
-        return redirect()->to('/subscriptions')->with('message', $data['message']['message']);
+        return redirect()->to('/premises')->with('message', $data['message']['message']);
     }
 
-    public function show($id){
+    public function show($id)
+    {
         $data['title'] = "Subscription";
-        $data['subscription'] = callAPI('/subscription/'.$id, 'get');
-        return view('subscription/show', $data);
+        $data['premise'] = callAPI('/premise/' . $id, 'get');
+        return view('premise/show', $data);
     }
+
 
 }
---
+++ app/Views/premises/form.php
@@ -0,0 +1,62 @@
+<?php
+helper('form');
+helper('url');
+
+    $hidden_input = [];
+    $url = uri_string();
+    $url = preg_replace('/[0-9]+/', '', $url);
+    if ($url == "subscription/edit/") {
+        $action = "subscription/edit/".$subscription['idsubscription'];
+    } else {
+        $action = "subscription/create";
+    }
+
+    echo form_open_multipart($action, 'id="subscription-create-form" class="subscription-card"', $hidden_input);
+
+                $value = (isset($subscription) ? $subscription['picture'] :'');
+                if ($value)
+                {
+                    echo '<img class="mb-3 img-fluid img-thumbnail"  alt="can\'t load picture" src="' . base_url("assets/images/subscriptions/" . $value) . '" />';
+                }
+
+                echo '<div class="form-group mb-3">';
+                            echo form_label('Subscription name' , "label-subscription-name");
+                            $value = (isset($subscription) ? $subscription['name'] :'');
+                            echo form_input(['type'  => 'text', 'name'  => 'name', 'class' => 'form-control', 'value' => $value, 'placeholder' => "Name of subscription", 'required' => 'required']);
+                echo '</div>';
+
+                echo '<div class="form-group mb-3">';
+                echo form_label('Subscription description' , "label-subscription-description");
+                $value = (isset($subscription) ? $subscription['description'] :'');
+                echo form_input(['type'  => 'textarea', 'name'  => 'description', 'class' => 'form-control', 'value' => $value, 'placeholder' => "Description of subscription", 'required' => 'required']);
+                echo '</div>';
+
+                echo '<div class="form-group mb-3">';
+                    echo form_label('Subscription price (€/month)' , "label-subscription-price");
+                    $value = (isset($subscription) ? $subscription['price'] :'');
+                    echo form_input(['type'  => 'numeric', 'name'  => 'price', 'class' => 'form-control', 'value' => $value,'placeholder' => "Price of subscription (€/month)", 'required' => 'required']);
+                echo '</div>';
+
+                echo '<div class="form-group mb-3">';
+                    echo form_label('Subscription max lessons' , "label-subscription-maxlessonaccess");
+                    $value = (isset($subscription) ? $subscription['maxlessonaccess'] :'');
+                    echo form_input(['type'  => 'numeric', 'name'  => 'maxlessonaccess', 'class' => 'form-control', 'value' => $value, 'placeholder' => "Maximum lesson viewable a day", 'required' => 'required']);
+                echo '</div>';
+
+                echo '<div class="form-group mb-3">';
+                    echo form_label('Subscription picture' , "label-subscription-picture");
+                    echo form_input(['type'  => 'file', 'name'  => 'picture', 'class' => 'form-control']);
+                echo '</div>';
+
+                echo '<div class="form-group mb-3">';
+                    echo form_submit('', 'Save', 'class="btn btn-primary mt-3"');
+                    //echo "<button type='button' onclick='document.location.href('/subscriptions')'>Cancel</button>";
+                echo '</div>';
+    echo form_close();
+
+    echo '</main>';
+    echo $this->include('layouts/footer')
+?>
+</body>
+<script src=<?= base_url('assets/js/create_users.js')?>></script>
+</html>
