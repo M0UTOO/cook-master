6725385bad85cefe7b9b16fc3132ed303ff02515
Mil0w0 <lhilderal@myges.fr>
2023-06-27 13:25:48 +0200

    can view reservations of rooms by clients

--- app/Views/cookingSpace/edit.php
+++ app/Views/cookingSpace/edit.php
@@ -6,5 +6,15 @@ echo '<body>';
 echo $this->include('layouts/header') ;
 
 echo "<h2>" . $title ."</h2>";
-
+echo "<section id='first'>";
 echo $this->include('cookingSpace/form') ;
+echo "</section>";
+
+echo $this->include('cookingSpace/reservations') ;
+
+echo '</main>';
+echo $this->include('layouts/footer')
+?>
+</body>
+<script src=<?= base_url('assets/js/create_users.js')?>></script>
+</html>
