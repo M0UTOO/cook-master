6caea8ec4483b19c272129992a3d9bebf60a24f9
M0UTOO <ninoplane@gmail.com>
2023-06-30 03:24:59 -0700

    wip on account page

--- app/Views/users/profile.php
+++ app/Views/users/profile.php
@@ -32,7 +32,7 @@
         // echo '<div class="d-flex flex-column">';
 
         // echo '</section>';
-        echo '<section>';
+        //echo '<section>';
 
         // echo '<div class="d-flex flex-row">';
         // echo '<div class="d-flex flex-column">';
@@ -55,13 +55,15 @@
         // echo '</div>';
         // echo '<div class="d-flex flex-column">';
 
-        // echo '</section>';
-        echo '<section class="account-cards">';
-        echo '<div class="d-flex flex-column">';
-        echo '<div class="d-flex flex-row">';
+        // echo '</section>'
+        echo '<section class="account-cards" style="min-width: 100%;">';
+        echo "<h1 class='mb-3'>" . $title . "</h1>";
+        echo '<div class="account-row">';
         $redirection = base_url("users/profile/comingEvents");
-        echo '<div class="d-flex flex-column coming-event-card" id="clickable-div" data-href='.$redirection.'>';
+        echo '<div class="d-flex flex-column account-event-card" id="clickable-div" data-href='.$redirection.'>';
+        echo '<div class="card-title">';
         echo '<h2 class="mb-3">Coming events</h2>';
+        echo '</div>';
         if (isset($comingEvents) && !empty($comingEvents)) {
             foreach ($comingEvents as $event) {
                 echo '<div class="d-flex flex-row">';
@@ -72,8 +74,118 @@
                 echo '</div>';
             }
         } else {
+            echo '<div style="min-width: 100%;">';
             echo '<p>No coming events</p>';
+            echo '</div>';
         }
+        echo '</div>';
+        $redirection = base_url("users/profile/pastEvents");
+        echo '<div class="d-flex flex-column account-event-card" id="clickable-div" data-href='.$redirection.'>';
+        echo '<div class="card-title">';
+        echo '<h2 class="mb-3">Past events</h2>';
+        echo '</div>';
+        if (isset($pastEvents) && !empty($pastEvents)) {
+            foreach ($pastEvents as $event) {
+                echo '<div class="d-flex flex-row">';
+                echo '<div class="d-flex flex-column">';
+                echo '<p>' . $event['name'] . '</p>';
+                echo '<p>' . $event['starttime'] . '</p>';
+                echo '</div>';
+                echo '</div>';
+            }
+        } else {
+            echo '<p>No past events</p>';
+        }
+        echo '</div>';
+        echo '</div>';
+        echo '</section>';
+
+        echo '<section class="account-cards" style="min-width: 100%;">';
+        echo '<div class="account-row">';
+        $redirection = base_url("users/profile/comingEvents");
+        echo '<div class="d-flex flex-column account-event-card" id="clickable-div" data-href='.$redirection.'>';
+        echo '<div class="card-title">';
+        echo '<h2 class="mb-3">My Comments</h2>';
+        echo '</div>';
+        if (isset($comments) && !empty($comments)) {
+            foreach ($comments as $event) {
+                echo '<div class="d-flex flex-row">';
+                echo '<div class="d-flex flex-column">';
+                echo '<p>' . $event['name'] . '</p>';
+                echo '<p>' . $event['starttime'] . '</p>';
+                echo '</div>';
+                echo '</div>';
+            }
+        } else {
+            echo '<div style="min-width: 100%;">';
+            echo '<p>No comments</p>';
+            echo '</div>';
+        }
+        echo '</div>';
+        $redirection = base_url("users/profile/pastEvents");
+        echo '<div class="d-flex flex-column account-event-card" id="clickable-div" data-href='.$redirection.'>';
+        echo '<div class="card-title">';
+        echo '<h2 class="mb-3">My Account</h2>';
+        echo '</div>';
+        if (isset($account) && !empty($account)) {
+            foreach ($account as $event) {
+                echo '<div class="d-flex flex-row">';
+                echo '<div class="d-flex flex-column">';
+                echo '<p>' . $event['name'] . '</p>';
+                echo '<p>' . $event['starttime'] . '</p>';
+                echo '</div>';
+                echo '</div>';
+            }
+        } else {
+            echo '<p>No Account</p>';
+        }
+        echo '</div>';
+        echo '</div>';
+        echo '</section>';
+
+        echo '<section class="account-cards" style="min-width: 100%;">';
+        echo '<div class="account-row">';
+        $redirection = base_url("users/profile/comingEvents");
+        echo '<div class="d-flex flex-column account-event-card" id="clickable-div" data-href='.$redirection.'>';
+        echo '<div class="card-title">';
+        echo '<h2 class="mb-3">My Bills</h2>';
+        echo '</div>';
+        if (isset($bills) && !empty($bills)) {
+            foreach ($bills as $event) {
+                echo '<div class="d-flex flex-row">';
+                echo '<div class="d-flex flex-column">';
+                echo '<p>' . $event['name'] . '</p>';
+                echo '<p>' . $event['starttime'] . '</p>';
+                echo '</div>';
+                echo '</div>';
+            }
+        } else {
+            echo '<div style="min-width: 100%;">';
+            echo '<p>No Bills</p>';
+            echo '</div>';
+        }
+        echo '</div>';
+        $redirection = base_url("users/profile/pastEvents");
+        echo '<div class="d-flex flex-column account-event-card" id="clickable-div" data-href='.$redirection.'>';
+        echo '<div class="card-title">';
+        echo '<h2 class="mb-3">My Formations</h2>';
+        echo '</div>';
+        if (isset($pastEvents) && !empty($pastEvents)) {
+            foreach ($pastEvents as $event) {
+                echo '<div class="d-flex flex-row">';
+                echo '<div class="d-flex flex-column">';
+                echo '<p>' . $event['name'] . '</p>';
+                echo '<p>' . $event['starttime'] . '</p>';
+                echo '</div>';
+                echo '</div>';
+            }
+        } else {
+            echo '<p>No Formations</p>';
+        }
+        echo '</div>';
+        echo '</div>';
+        echo '</section>';
+
 
         ?>
 
