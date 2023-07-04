86f05ff4df70b4777900911a2f6c266174167760
Mil0w0 <lhilderal@myges.fr>
2023-07-04 20:58:57 +0200

    I CAN DISPLAY A RESERVATION ON CALENDAR WOUHOU

--- app/Views/cookingSpace/availability.php
+++ app/Views/cookingSpace/availability.php
@@ -2,6 +2,7 @@
 
 echo "<h2 class='mt-4'>Availabilty</h2>";
 
+echo "<p id='test'></p>";
 echo "<div id='calendar' class='mt-4 room-calendar'></div>";
 
 ?>
@@ -13,11 +14,17 @@ echo "<div id='calendar' class='mt-4 room-calendar'></div>";
     let reservations = [];
     //foreach reservations fill the array with the reservations
     <?php
+        //in 2023-05-31 22:27:34 take the space and replace it by a T with another function in php
+
+
     foreach ($reservations as $reservation){
-        echo "reservations.push({title: 'ReservationX','allDay': false, start: '".$reservation['starttime']."', end: '".$reservation['endtime']."'});";
+        //CHANGE DATE FORMAT BEACAUSE OF THE LIBRARY : ISO 8601
+        $startTime= (new DateTime($reservation['starttime']))->format('Y-m-d\TH:i:s');
+        $endTime= (new DateTime($reservation['endtime']))->format('Y-m-d\TH:i:s');
+        echo "reservations.push({title: 'Client reservation',color: '#4D47A7','allDay': false, start: '".$startTime."', end: '".$endTime."'});";
     }
     ?>
-    reservations.push({title: 'ReservationX', start: '2023-07-08', end: '2023-07-09'});
+    reservations.push({title: 'Client reservation', start: '2023-07-08T12:00:00', end: '2023-07-08T16:00:00', color: '#4D47A7'});
     console.log(reservations);
 
     document.addEventListener('DOMContentLoaded', function() {
@@ -26,21 +33,23 @@ echo "<div id='calendar' class='mt-4 room-calendar'></div>";
             initialView: 'timeGridWeek',
             allDaySlot: false,
             slotMinTime: "07:00:00",
-            slotMaxTime: "19:00:00",
+            slotMaxTime: "21:00:00",
             themeSystem: 'bootstrap5',
             timeZone: 'UTC',
             businessHours: {
                 // days of week. an array of zero-based day of week integers (0=Sunday)
                 daysOfWeek: [ 0, 1, 2, 3, 4, 5, 6 ],
-
-                startTime: '9:00',
-                endTime: '18:00',
+                startTime: '8:00',
+                endTime: '20:00',
             },
             events: reservations,
-            // headerToolbar: {
-            //     left: 'prev,next',
-            //     center: 'title',
-            // }
+            headerToolbar: {
+                left: 'title',
+            },
+            eventClick: function(info) {
+                alert('Event: ' + info.event.title);
+                //info.el.style.borderColor = 'green';
+            }
         });
         calendar.render();
     });
