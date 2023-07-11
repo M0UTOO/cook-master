f5e84cef142e4ae07a91e21c6485b066ee792976
M0UTOO <ninoplane@gmail.com>
2023-07-11 03:53:22 -0700

    fix chat

--- app/Views/message/show.php
+++ app/Views/message/show.php
@@ -24,8 +24,6 @@ echo $this->include('layouts/head') ;
     </body>
 <script src=<?= base_url('assets/js/tables.js')?>></script>
 <script>
-const socket = new WebSocket('ws://localhost:8081');
-
 const id = <?php 
         if (isset($idcontractor)) {
             echo $idcontractor;
@@ -37,19 +35,37 @@ const userId = <?php echo json_encode(session()->get('id')); ?>;
 
 const url = `http://localhost:9000/message/${id}/${userId}`;
 
-socket.addEventListener('open', () => {
-    console.log('Connected to WebSocket server');
-});
+// socket.addEventListener('open', () => {
+//     console.log('Connected to WebSocket server');
+// });
 
-socket.addEventListener('message', (event) => {
-    const message = JSON.parse(event.data);
-    messages.push(message);
-    renderMessages();
+// socket.addEventListener('message', (event) => {
+//     const message = JSON.parse(event.data);
+//     messages.push(message);
+//     renderMessages();
 
-    if (message.idsender === id) {
+//     if (message.idsender === id) {
 
-        const data = {
-            content: message.content,
+//         const data = {
+//             content: message.content,
+//         };
+
+//     }
+// });
+
+// socket.addEventListener('close', () => {
+//     console.log('Disconnected from WebSocket server');
+// });
+
+// Sending a message when the button is clicked
+const sendButton = document.getElementById('send-button');
+sendButton.addEventListener('click', () => {
+    const messageInput = document.getElementById('message-input');
+    const messageContent = messageInput.value.trim();
+
+    if (messageContent !== "") {
+        const message = {
+            content: messageContent
         };
 
         const options = {
@@ -58,7 +74,7 @@ socket.addEventListener('message', (event) => {
                 'Content-Type': 'application/json',
                 'Token': '<?php echo env('API_TOKEN'); ?>'
             },
-            body: JSON.stringify(data),
+            body: JSON.stringify(message),
         };
 
         fetch(url, options)
@@ -69,37 +85,20 @@ socket.addEventListener('message', (event) => {
             .catch(error => {
                 console.log(error);
             });
-    }
-});
 
-socket.addEventListener('close', () => {
-    console.log('Disconnected from WebSocket server');
-});
-
-// Sending a message when the button is clicked
-const sendButton = document.getElementById('send-button');
-sendButton.addEventListener('click', () => {
-    const messageInput = document.getElementById('message-input');
-    const messageContent = messageInput.value.trim();
-
-    if (messageContent !== "") {
-        const message = {
-            content: messageContent,
-            createdAt: formatDate(),
-            idsender: id
-        };
-        socket.send(JSON.stringify(message));
+        message.idsender = id;
+        messages.push(message);
+        renderMessages();
 
         // Clear the input field
         messageInput.value = '';
     }
 });
 
-const messages = <?php echo json_encode($messages); ?>;
-
-
+var messages = <?php echo json_encode($messages); ?>;
 const messageList = document.getElementById("message-list");
 const messageInput = document.getElementById("message-input");
+setInterval(getMessages, 1000);
 
 // Function to render messages
 function renderMessages() {
@@ -139,6 +138,30 @@ function formatDate() {
     const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 
     return formattedDate;
+}
+
+function getMessages() {
+    const options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Token': '<?php echo env('API_TOKEN'); ?>'
+            },
+        };
+
+        fetch(url, options)
+            .then(response => response.json())
+            .then(data => {
+                if (data == null) {
+                    messages = [{"idmessage":0,"content":"Start to type to chat with the other !","createdat":"2020-01-01 22:22:22","idsender":0,"idreceiver":0}];
+                } else {
+                    messages = data;
+                }
+                renderMessages();
+            })
+            .catch(error => {
+                console.log(error);
+            });
 }
 
 // Call the renderMessages function to initially display the messages
